e2902e0dc46a99f0a034faa913c8339de097c281
Vishal Sharma <39647664+vsvishal@users.noreply.github.com>
2023-10-09 18:04:13 +0530

    login image updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import "./App.css";
 import Body from "./components/Body";
 
 function App() {
   return (
-    <div className="text-3xl bg-slate-500">
-      <h1>Vishal</h1>
+    <div className="">
       <Body />
     </div>
   );
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,18 @@
 import React from "react";
+import Header from "./Header";
 
 function Login() {
-  return <div>Login</div>;
+  return (
+    <div className="absolute">
+      <Header />
+      <div>
+        <img
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/893a42ad-6a39-43c2-bbc1-a951ec64ed6d/1d86e0ac-428c-4dfa-9810-5251dbf446f8/IN-en-20231002-popsignuptwoweeks-perspective_alpha_website_large.jpg"
+          alt=""
+        />
+      </div>
+    </div>
+  );
 }
 
 export default Login;
